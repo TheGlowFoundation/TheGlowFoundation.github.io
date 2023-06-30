48c241278890ebbdd167b83def1c73789af1b329
TheGlowFoundation <132367324+TheGlowFoundation@users.noreply.github.com>
2023-06-30 18:35:40 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,29 +1,11 @@
-document.addEventListener("DOMContentLoaded", function() {
-  const slides = document.querySelector(".slides");
-  const images = slides.querySelectorAll("img");
-  const sliderButtons = document.querySelector(".slider-buttons");
+const  navOpen = document.getElementById('nav-bar');
+navOpen.style.display='none';
 
-  // Generate buttons for each slide
-  for (let i = 0; i < images.length; i++) {
-    const button = document.createElement("button");
-    button.addEventListener("click", function() {
-      goToSlide(i);
-    });
-    sliderButtons.appendChild(button);
-  }
+function openNav() {
+    navOpen.style.display='flex';
+   // navOpen style.zIndex='2'
+}
 
-  const buttons = sliderButtons.querySelectorAll("button");
-
-  // Add active class to the first button
-  buttons[0].classList.add("active");
-
-  // Go to the selected slide
-  function goToSlide(index) {
-    slides.style.transform = `translateX(-${index * 100}%)`;
-
-    // Update active button
-    buttons.forEach(button => button.classList.remove("active"));
-    buttons[index].classList.add("active");
-  }
-});
-                            
+function closeNav() {
+    navOpen.style.display='none';
+}
