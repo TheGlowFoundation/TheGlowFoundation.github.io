16170f0feb5d624513e74a0cf918d8aa6f5830e3
TheGlowFoundation <132367324+TheGlowFoundation@users.noreply.github.com>
2023-07-03 09:18:24 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
+//opening and closing the navigation bar by clicking the page title 
 const  navOpen = document.getElementById('nav-bar');
 navOpen.style.display='none';
-
 function openNav() {
     navOpen.style.display='flex';
-   // navOpen style.zIndex='2'
 }
-
 function closeNav() {
     navOpen.style.display='none';
 }
+
