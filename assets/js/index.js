d48f25dd2bab0f8adbfb2b6cad579252d9c212ea
Lazaro Osee <132367324+TheGlowFoundation@users.noreply.github.com>
2024-08-02 19:31:07 +0800

    creating styles

---
+++ assets/js/index.js
@@ -0,0 +1,13 @@
+
+// Initialize script when all document body is loaded
+document.addEventListener('DOMContentLoaded', init(window, document))
+
+/**
+ * Initiates the JavaScript functions
+ * @param {Window} window 
+ * @param {Document} document 
+ */
+const init = (window, document) => {
+    const win = window;
+    const doc = document;
+}
